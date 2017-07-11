663e6c24f8b11994bc6598e11c0063776908ec62
meganho <bbeeaanns@gmail.com>
2017-07-11 16:22:33 +0200

    Implement easy chat

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friendships
    resources :users, only: [:index]
+
+   resources :conversations do
+        resources :messages
+      end
+
+
+
   root "friendships#index"
 end
